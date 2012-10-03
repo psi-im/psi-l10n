e5b46afed14912c1baef3109ffe18858208acbc9
Justin Karneges <justin@affinix.com>
2012-10-03 15:49:20 -0700

    script for compiling all language packs

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -rf dist
+mkdir dist
+
+for lang in `cat langs`; do
+	if [ -f $QTDIR/translations/qt_$lang.qm ]; then
+		cp $QTDIR/translations/qt_$lang.qm dist
+	fi
+
+	if [ -f $lang/qt_$lang.ts ]; then
+		lrelease $lang/qt_$lang.ts -qm dist/qt_$lang.qm
+	fi
+
+	lrelease $lang/psi_$lang.ts -qm dist/psi_$lang.qm
+done
