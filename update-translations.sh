234ee90854a50da0c8c9ad7d49f9369c4089b49c
Boris Pek <tehnick-8@yandex.ru>
2020-09-06 21:17:17 +0300

    Update update-translations.sh

--- update-translations.sh
+++ update-translations.sh
@@ -179,7 +179,7 @@ case "${1}" in
     cp -a "${PSIMEDIA_DIR}"/psiplugin "psimedia/"
 
     cd "${PSI_DIR}/src"
-    python2 ../admin/update_options_ts.py ../options/default.xml > \
+    python3 ../admin/update_options_ts.py ../options/default.xml > \
         "${CUR_DIR}/tmp/option_translations.cpp"
     # ending of magical hack
 
